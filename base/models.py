60524f0319d8ee80a4e923120b4495e0918bfeee
Athika Priya <athikapriya1997@gmail.com>
2026-01-31 23:13:50 +0600

    Set Room model ordering by updated and created date

--- base/models.py
+++ base/models.py
@@ -22,6 +22,9 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta :
+        ordering = ["-updated", "-created_at"]
+
     def __str__(self):
         return self.name
     
